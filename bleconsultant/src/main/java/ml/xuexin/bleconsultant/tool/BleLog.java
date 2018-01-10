5586a173652b231b05b2707b4532932a18d2311f
廖宇浩 <taiyangniaoming@foxmail.com>
2018-01-10 11:52:11 +0800

    修复log, message可能为空的问题

--- bleconsultant/src/main/java/ml/xuexin/bleconsultant/tool/BleLog.java
+++ bleconsultant/src/main/java/ml/xuexin/bleconsultant/tool/BleLog.java
@@ -11,24 +11,29 @@ public class BleLog {
     public static final String TAG = "BleConsultant";
 
     public static void e(String msg) {
-        Log.e(TAG, msg);
+        if (msg != null)
+            Log.e(TAG, msg);
     }
 
     public static void d(String msg) {
-        if (DEBUG) Log.d(TAG, msg);
+        if (msg != null)
+            if (DEBUG) Log.d(TAG, msg);
     }
 
     public static void w(String msg) {
-        if (DEBUG) Log.w(TAG, msg);
+        if (msg != null)
+            if (DEBUG) Log.w(TAG, msg);
     }
 
     public static void i(String msg) {
-        if (DEBUG) Log.i(TAG, msg);
+        if (msg != null)
+            if (DEBUG) Log.i(TAG, msg);
     }
 
 
     /**
      * parse byte[] to hex string
+     *
      * @param data
      * @return
      */
